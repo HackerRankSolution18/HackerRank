c4da82a859a659b480d5a34deefb4be0d8532ea2
maheshbabu-b <maheshbabu26588@gmail.com>
2023-05-12 17:30:49 +0530

    Welcome to Java

--- src/main/java/JavaIfElse.java
+++ src/main/java/JavaIfElse.java
@@ -2,21 +2,27 @@ import java.util.Scanner;
 
 public class JavaIfElse {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        int N=scanner.nextInt();
-        if(N%2!=0){
-            System.out.println("Weird");
-        }
-        else {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        String result = checkNumber(N);
+        System.out.println(result);
+    }
+
+    public static String checkNumber(int N) {
+        if (N % 2 != 0) {
+            return "Weird";
+        } else {
             if (N >= 2 && N <= 5) {
-                System.out.println("Not Weird");
+                return "Not Weird";
             } else {
                 if (N >= 6 && N <= 20) {
-                    System.out.println("Weird");
+                    return "Weird";
                 } else {
-                    System.out.println("Not Weird");
+                    return "Not Weird";
                 }
             }
         }
     }
 }
+
+
--- src/test/java/JavaIfElseTest.java
+++ src/test/java/JavaIfElseTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,10 +9,19 @@ class JavaIfElseTest {
 
     @Test
     public void ifelseEven2to5(){
-       int N = 50;
+        int input = 3;
+        String expected = "Weird";
+        String actual = JavaIfElse.checkNumber(input);
+        Assertions.assertEquals(expected, actual);
 
+    }
 
-
+    @Test
+    public void ifelseEven6to10(){
+        int input = 2;
+        String expected = "Not Weird";
+        String actual = JavaIfElse.checkNumber(input);
+        Assertions.assertEquals(expected, actual);
     }
 
 }
